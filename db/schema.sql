c446b18d7f26d6ea3dbe1f34109f355a511b4458
Sam Craig <samfcraig@gmail.com>
2023-01-17 20:47:23 -0500

    caught missed areas in schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,22 @@ CREATE DATABASE IF NOT EXISTS employee_db;
 
 USE employee_db;
 
-CREATE TABLE department ();
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
 
-CREATE TABLE role ();
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT
+);
 
-CREATE TABLE employee ();
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
