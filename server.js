46fcc0ff8d5544bcf30c0917609d248f4b7e09c1
Sam Craig <samfcraig@gmail.com>
2023-01-17 21:49:50 -0500

    my displayed tables are showing everything 4 times. Debugging

--- server.js
+++ server.js
@@ -9,21 +9,42 @@ const db = mysql.createConnection({
 });
 
 chooseOption = (type) => {
-switch (type) {
-    case 'VIEW ALL EMPLOYEES': {
-        db.query('SELECT * FROM employee', (err, employee) => {
-            console.table(employee);
-        });
+    switch (type) {
+        case 'VIEW ALL EMPLOYEES': {
+            db.query('SELECT * FROM employee', (err, employee) => {
+                console.table(employee);
+                init();
+            });
+        }
+        case 'VIEW ALL DEPARTMENTS': {
+            db.query('SELECT * FROM department', (err, department) => {
+                console.table(department);
+                init();
+            });
+        }
+        case 'VIEW ALL ROLES': {
+            db.query('SELECT * FROM role', (err, roles) => {
+                console.table(roles);
+                init();
+            });
+        }
     }
-    case 'VIEW ALL DEPARTMENTS': {
-        db.query('SELECT * FROM department', (err, department) => {
-            console.table(department);
-        });
-    }
-    case 'VIEW ALL ROLES': {
-        db.query('SELECT * FROM role', (err, roles) => {
-            console.table(roles);
-        });
-    };
-};
 }
+
+const init = () => {
+prompt({
+    type: 'rawlist',
+    message: 'Choose one of the following categories to view',
+    name: 'type',
+    choices: [
+        'VIEW ALL EMPLOYEES',
+        'VIEW ALL DEPARTMENTS',
+        'VIEW ALL ROLES',
+    ],
+})
+.then((answers) => {
+    chooseOption(answers.type)
+});
+}
+
+init();
