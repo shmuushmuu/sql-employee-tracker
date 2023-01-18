c7f476721379d4c1b2e9fe60b1434551420d8112
Sam Craig <samfcraig@gmail.com>
2023-01-17 21:41:18 -0500

    set up chooseOption function with switch types

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
 const mysql = require('mysql2');
+require('console.table');
 
 const db = mysql.createConnection({
     user: "root",
     database: "employee_db",
 });
 
-db.query(' SELECT * FROM employee', (err, employee) => {
-    console.table(employee);
-});
-db.query(' SELECT * FROM department', (err, department) => {
-    console.table(department);
-});
-db.query(' SELECT * FROM role', (err, roles) => {
-    console.table(roles);
-});
+chooseOption = (type) => {
+switch (type) {
+    case 'VIEW ALL EMPLOYEES': {
+        db.query('SELECT * FROM employee', (err, employee) => {
+            console.table(employee);
+        });
+    }
+    case 'VIEW ALL DEPARTMENTS': {
+        db.query('SELECT * FROM department', (err, department) => {
+            console.table(department);
+        });
+    }
+    case 'VIEW ALL ROLES': {
+        db.query('SELECT * FROM role', (err, roles) => {
+            console.table(roles);
+        });
+    };
+};
+}
