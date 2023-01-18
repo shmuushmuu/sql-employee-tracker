c7c9748e2fe60dfa60e7f5e99dfbd901421eac94
Sam Craig <samfcraig@gmail.com>
2023-01-17 20:43:36 -0500

    set up schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE IF NOT EXISTS employee_db;
+
+USE employee_db;
+
+CREATE TABLE department ();
+
+CREATE TABLE role ();
+
+CREATE TABLE employee ();
