b3d72ac517ce28292a65714e670cd09428719300
Sam Craig <samfcraig@gmail.com>
2023-01-21 18:25:55 -0500

    adding 'add employee' function

--- server.js
+++ server.js
@@ -6,7 +6,24 @@ require('console.table');
 const db = mysql.createConnection({
     user: "root",
     database: "employee_db",
-});
+},
+console.log(`Connected to the employee_db`)
+);
+
+const addEmployee = async () => {
+    const [responses] = await db.promise().query('SELECT id, title FROM roles');
+    for (let i = 0; i < responses.length; i++) {
+        let role = responses[i].title;
+        roles.push(role);
+    } 
+    id = results;
+    const [employeeResults] = await db.promise().query('SELECT id, first_name, last_name FROM employees');
+    for (let i = 0; i < employeeResults.length; i++) {
+        let fullName = employeeResults[i].first_name + ' ' + employeeResults[i].last_name;
+        employeeResults.push(fullName);
+    }
+
+}
 
 const chooseOption = (type) => {
     switch (type) {
@@ -15,36 +32,47 @@ const chooseOption = (type) => {
                 console.table(employee);
                 init();
             });
+            break;
         }
         case 'VIEW ALL DEPARTMENTS': {
             db.query('SELECT * FROM department', (err, department) => {
                 console.table(department);
                 init();
             });
+            break;
         }
         case 'VIEW ALL ROLES': {
             db.query('SELECT * FROM role', (err, roles) => {
                 console.table(roles);
                 init();
             });
+            break;
+        }
+        case 'ADD EMPLOYEE': {
+            db.query('INSERT INTO employee', )
+        }
+        case 'FIND TOTAL SALARIES PER DEPO': {
+            db.query('SELECT SUM(quantity) AS total_in_salaries FROM department GROUP BY section', function (err, results) {
+                console.log(results);
+              });
         }
     }
 }
 
 const init = () => {
-prompt({
-    type: 'rawlist',
-    message: 'Choose one of the following categories to view',
-    name: 'type',
-    choices: [
-        'VIEW ALL EMPLOYEES',
-        'VIEW ALL DEPARTMENTS',
-        'VIEW ALL ROLES',
-    ],
-})
-.then((answers) => {
-    chooseOption(answers.type)
-});
+    prompt({
+        type: 'rawlist',
+        message: 'Choose one of the following categories to view',
+        name: 'type',
+        choices: [
+            'VIEW ALL EMPLOYEES',
+            'VIEW ALL DEPARTMENTS',
+            'VIEW ALL ROLES',
+        ],
+    })
+        .then((answers) => {
+            chooseOption(answers.type)
+        });
 }
 
 init();
